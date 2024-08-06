f367fd8a6dca93508ea047dc9b686b68d78d6b04
OskarMartens <martens.oskar@gmail.com>
2024-08-06 13:57:28 +0200

    Able to log data in CreateTodo

--- src/components/create-todo/CreateTodo.tsx
+++ src/components/create-todo/CreateTodo.tsx
@@ -1,14 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./CreateTodo.css";
+import { ITodo } from "../../interface";
 
 export default function CreateTodo() {
+  const [todoData, setTodoData] = useState<ITodo>({
+    assignedTo: "",
+    content: "",
+    timeStamp: new Date().toLocaleString(),
+  });
+
+  const saveTodo = () => {
+    setTodoData({...todoData, timeStamp: new Date().toLocaleString()})
+    console.log(todoData);
+  }
+
   return (
     <div className="create-main">
       <h2>Create Todo</h2>
-      <input type="text" placeholder="What needs to be done?" />
-      <input type="text" placeholder="Who's doing it?"></input>
-      <button>Save todo</button>
+      <input
+        type="text"
+        placeholder="What needs to be done?"
+        onChange={(e) => setTodoData({ ...todoData, content: e.target.value })}
+      />
+      <input type="text" placeholder="Who's doing it?" 
+      onChange={(e) => setTodoData({...todoData, assignedTo: e.target.value})}/>
+      <button onClick={saveTodo}>Save todo</button>
     </div>
   );
 }
