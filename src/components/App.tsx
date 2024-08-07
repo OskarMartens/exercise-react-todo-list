1c538358b4ad6b48b557c2a62d02859ed1d77981
OskarMartens <martens.oskar@gmail.com>
2024-08-07 12:28:51 +0200

    Can now remove todos

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -7,12 +7,16 @@ import data from "../data/todos.json";
 export function App() {
   const [todos, setTodos] = useState<ITodo[]>(data as ITodo[]);
 
+  const removeTodo = (todo: ITodo): void  => {
+    setTodos(todos.filter((t) => t !== todo))
+  };
+
   return (
     <>
       <main className="app-main">
         <CreateTodo />
         {todos?.map((todo) => (
-          <TodoCard key={todo.content} data={todo} />
+          <TodoCard key={todo.content} todo={todo} onClick={removeTodo} />
         ))}
       </main>
     </>
--- src/components/todo-card/TodoCard.tsx
+++ src/components/todo-card/TodoCard.tsx
@@ -4,23 +4,24 @@ import "./TodoCard.css";
 import { ITodo } from "../../interface";
 
 interface ITodoProps{
-  data: ITodo;
+  todo: ITodo;
+  onClick: (todo: ITodo) => void;
 };
 
-export default function TodoCard({data}: ITodoProps) {
+export default function TodoCard({todo, onClick}: ITodoProps) {
   return (
     <div className="todo-card-main">
       <section className="top-section">
-        <p>{data.assignedTo}</p>
+        <p>{todo.assignedTo}</p>
       </section>
       <section className="middle-section">
-        <h2 className="header">{data.content}</h2>
+        <h2 className="header">{todo.content}</h2>
       </section>
       <section className="bottom-section">
-        <p>{data.timeStamp}</p>
+        <p>{todo.timeStamp}</p>
         <section className="buttons">
-          <button>Remove</button>
-          <button>Mark as done</button>
+          <button onClick={() => onClick(todo)}>Remove</button>
+          <button onClick={() => onClick(todo)}>Mark as done</button>
         </section>
       </section>
     </div>
